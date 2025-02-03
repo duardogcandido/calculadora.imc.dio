5ad40558dad7bde73b4028e4f54e21e12b19d35d
Eduardo <eduardo.gcandido@outlook.com>
2025-02-03 19:39:31 -0300

    classificação imc

--- console/lib/service/imc_service.dart
+++ console/lib/service/imc_service.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:console/model/pessoa.dart';
 
@@ -9,6 +10,7 @@ class ImcService{
     Pessoa pessoa = setPessoa(nome, peso, altura);
     double imc = calcularImc(pessoa);
     imprimirMensagem('${pessoa.nome}, seu IMC é: $imc');
+    imprimirMensagem(classificacaoImc(imc));
   }
 
   static void imprimirMensagem(String mensagem){
@@ -50,5 +52,29 @@ class ImcService{
 
   static double calcularImc(Pessoa pessoa){
     return pessoa.peso / (pessoa.altura * pessoa.altura);
+  }
+
+  static String classificacaoImc(double imc){
+    String classificacao = '';
+
+    if(imc < 16){
+      classificacao = 'Magreza grave';
+    }else if(imc < 17){
+      classificacao = 'Magreza moderada';
+    }else if(imc < 18.5){
+      classificacao = 'Magreza leve';
+    }else if(imc < 25){
+      classificacao = 'Saudável';
+    }else if(imc < 30){
+      classificacao = 'Sobrepeso';
+    }else if(imc < 35){
+      classificacao = 'Obesidade Grau I';
+    }else if(imc < 40){
+      classificacao = 'Obesidade Grau II (severa)';
+    }else{
+      classificacao = 'Obesidade Grau III (mórbida)';
+    }
+
+    return 'Sua classificação de IMC é: $classificacao';
   }
 }
--- console/test/console_test.dart
+++ console/test/console_test.dart
@@ -1,11 +1,9 @@
-import 'dart:mirrors';
-
 import 'package:console/model/pessoa.dart';
 import 'package:console/service/imc_service.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Calculo do IMC', (){
+  group('Calculo do IMC Tests', (){
     test('Calcular IMC passando uma pessoa', () {
       Pessoa pessoa = Pessoa(nome: 'Fulano', peso: 70, altura: 1.70);
       expect(ImcService.calcularImc(pessoa), greaterThan(24));
@@ -28,5 +26,38 @@ void main() {
         reason: 'Pesoo não pode ser 0'
       );
     }); 
+  });
+  group('IMC Classification Tests', () {
+    test('IMC < 16', () {
+      expect(ImcService.classificacaoImc(15), 'Sua classificação de IMC é: Magreza grave');
+    });
+
+    test('IMC < 17', () {
+      expect(ImcService.classificacaoImc(16.5), 'Sua classificação de IMC é: Magreza moderada');
+    });
+
+    test('IMC < 18.5', () {
+      expect(ImcService.classificacaoImc(18), 'Sua classificação de IMC é: Magreza leve');
+    });
+
+    test('IMC < 25', () {
+      expect(ImcService.classificacaoImc(24), 'Sua classificação de IMC é: Saudável');
+    });
+
+    test('IMC < 30', () {
+      expect(ImcService.classificacaoImc(29), 'Sua classificação de IMC é: Sobrepeso');
+    });
+
+    test('IMC < 35', () {
+      expect(ImcService.classificacaoImc(34), 'Sua classificação de IMC é: Obesidade Grau I');
+    });
+
+    test('IMC < 40', () {
+      expect(ImcService.classificacaoImc(39), 'Sua classificação de IMC é: Obesidade Grau II (severa)');
+    });
+
+    test('IMC >= 40', () {
+      expect(ImcService.classificacaoImc(40), 'Sua classificação de IMC é: Obesidade Grau III (mórbida)');
+    });
   });
 }
